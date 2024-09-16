95742a050a9e666fed8396a9024110940f62fcc9
gabibrevi <gabibrevi@gmail.com>
2024-09-16 19:47:59 +0200

    CORS added

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,31 @@
+<<<<<<< Updated upstream
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
+=======
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
+import { connectDB } from "./config/db";
+import projectRoutes from "./routes/projectRoutes";
+>>>>>>> Stashed changes
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
+app.use(cors(corsConfig));
 
+<<<<<<< Updated upstream
 export default app
+=======
+//To read json on body http requests
+app.use(express.json());
+
+//Routes
+app.use("/api/projects", projectRoutes);
+
+export default app;
+>>>>>>> Stashed changes
